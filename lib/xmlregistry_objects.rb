73cb293869101d929a259d66c3b0250f9f28d063
James Robertson <digital.robertson@gmail.com>
2022-02-22 20:34:23 +0000

    minor improvement: Replaced RXFHelper with RXFReader.

--- lib/xmlregistry_objects.rb
+++ lib/xmlregistry_objects.rb
@@ -5,6 +5,7 @@
 
 require 'polyrex'
 require 'dws-registry'
+require 'rxfreader'
 
 
 class XMLRegistryObjects
@@ -15,14 +16,14 @@ class XMLRegistryObjects
 
     @log = log
     log.info 'XMLRegistryObjects/initialize: active' if log
-    
-    polyrexdoc = if obj.is_a? Polyrex then 
-    
+
+    polyrexdoc = if obj.is_a? Polyrex then
+
       obj
-    
+
     elsif obj.is_a? String then
-      
-      buffer, type = RXFHelper.read obj
+
+      buffer, type = RXFReader.read obj
 
       if type == :url then
         Polyrex.new buffer
@@ -31,9 +32,9 @@ class XMLRegistryObjects
             "methodx[name,subkeyname]' delimiter=' = '?>\n" + obj
       end
     end
-        
+
     log.info 'XMLRegistryObjects/initialize: before @to_h' if log
-    
+
     @to_h = polyrexdoc.records.inject({}) do |rtn, row|
 
       name, path = row.name, row.regkey[1..-2]
@@ -44,23 +45,23 @@ class XMLRegistryObjects
         def initialize(reg)
           @reg = reg
         end
-        
+
         def last_modified()
           lm = @reg.get_key('#{path}').attributes[:last_modified]
           Time.parse(lm) if lm
         end
-        
-        def set(h)          
-          
+
+        def set(h)
+
           h.each {|key,value| self.method((key.to_s + '=').to_sym).call value }
-          
+
         end
       "
 
       s = if row.records.any? then
 
         row.records.inject(base_methods) do |r, attr|
-          
+
           attr_name, subkey = attr.name, attr.subkeyname[/\w+$/]
           key = path + '/' + subkey
 
@@ -72,60 +73,60 @@ class XMLRegistryObjects
       else
 
         a = reg.xpath("#{path}/*")
-        
-        a.inject(base_methods) do |r, x| 
+
+        a.inject(base_methods) do |r, x|
 
           methods_name = subkey = x.name
 
-          type = x.attributes[:type]          
+          type = x.attributes[:type]
           key = path + '/' + subkey
-          
-          r << make_setdef(key, method_name=subkey)          
 
-          method_name += '?' if type and type == 'boolean'          
+          r << make_setdef(key, method_name=subkey)
+
+          method_name += '?' if type and type == 'boolean'
           r << make_def(key, method_name)
 
         end
       end
-      
+
       log.info 'XMLRegistryObjects/initialize: before class_eval ' + klass.to_s if log
-      
+
       klass.class_eval s
       rtn.merge name.to_sym => klass.new(reg)
 
     end
-    
+
     log.info 'XMLRegistryObjects/initialize: completed' if log
   end
-  
+
   def define_methods()
     @to_h.each.map {|k,v| "define_method :#{k.to_s}, ->{h[:#{k}]}"}.join("\n")
   end
-  
-  # to use this method the client is expect to  fetch the 
-  # hash object from the registry object e.g. 
+
+  # to use this method the client is expect to  fetch the
+  # hash object from the registry object e.g.
   #    xro = XMLRegistryObjects.new(reg,list)
   #    @h = xro.to_h
-  
+
   def define_methods2(obj=nil)
-    
+
     a = @to_h.each.map {|k,v| [k.to_s.to_sym, ->{@h[k.to_sym]} ] }
-    
+
     if obj then
-      
+
       a.each {|name, blk| obj.class.send(:define_method, name, blk) }
-      
+
     else
-      
+
       return a
-      
+
     end
-    
-  end  
+
+  end
 
   private
 
-  def make_def(key, method_name)    
+  def make_def(key, method_name)
 <<-EOF
 def #{method_name}()
   @reg.get_key('#{key}', auto_detect_type: true)
@@ -133,9 +134,9 @@ end
 
 EOF
   end
-  
-  def make_setdef(key, method_name)    
+
+  def make_setdef(key, method_name)
     "def #{method_name}=(v)   @reg.set_key '#{key}', v  end\n"
-  end  
+  end
 
 end
--- xmlregistry_objects.gemspec
+++ xmlregistry_objects.gemspec
@@ -1,15 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'xmlregistry_objects'
-  s.version = '0.7.5'
+  s.version = '0.8.0'
   s.summary = 'Query the registry using objects built dynamically from a ' + 
       'string which maps each object to a registry key.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/xmlregistry_objects.rb']
-  s.add_runtime_dependency('dws-registry', '~> 0.4', '>=0.4.0') 
-  s.add_runtime_dependency('polyrex', '~> 1.2', '>=1.2.0')
+  s.add_runtime_dependency('dws-registry', '~> 0.5', '>=0.5.0')
+  s.add_runtime_dependency('polyrex', '~> 1.4', '>=1.4.0')
   s.signing_key = '../privatekeys/xmlregistry_objects.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/xmlregistry_objects'
 end
